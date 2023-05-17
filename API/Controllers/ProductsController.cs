4878a1b0843c9d5b91e6e361546aeedf01c61253
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-05-17 18:21:37 +0100

    Add ProductBrand and ProductType endpoints

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
         {
             var products = await _productRepository.GetProductsAsync();
 
@@ -27,6 +27,18 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _productRepository.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productRepository.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productRepository.GetProductTypesAsync());
         }
     }
 }
