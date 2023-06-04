64ff64746cb36c766f063bba13edcc42a4525c6d
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-06-04 14:50:29 +0100

    Refactor Migration calling

---
+++ API/Extensions/DatabaseMigrationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class DatabaseMigrationExtensions
+    {
+        public static async Task<IApplicationBuilder> MigrateAsync(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<StoreContext>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                await context.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            return app;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -6,10 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<StoreContext>(
@@ -22,7 +19,6 @@ builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerDocumentation();
@@ -40,19 +36,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-var context = services.GetRequiredService<StoreContext>();
-var logger = services.GetRequiredService<ILogger<Program>>();
-
-try
-{
-    await context.Database.MigrateAsync();
-    await StoreContextSeed.SeedAsync(context);
-}
-catch (Exception ex)
-{
-    logger.LogError(ex, "An error occured during migration");
-}
+await app.MigrateAsync();
 
 app.Run();
