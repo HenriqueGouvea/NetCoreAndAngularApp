cc5cf768d66d54234d87d603e2feb80c04e46ab8
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-06-22 16:36:03 +0100

    Call the API to return products and render them

--- Presentation/src/app/app.component.ts
+++ Presentation/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Presentation';
+  products: any[] | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7043/api/products').subscribe((response: any) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
