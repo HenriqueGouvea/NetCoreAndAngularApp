c3e35ae8c9a101cba475512177c6baa54b209aac
Henrique Guilherme Gouvêa <henriquegouvea@hotmail.com>
2023-12-06 12:18:42 +0000

    Use the new shop component in the app component

--- Presentation/src/app/app.component.ts
+++ Presentation/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './shared/models/product';
-import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +7,9 @@ import { IPagination } from './shared/models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Presentation';
-  products: IProduct[] | undefined;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-      this.http.get('https://localhost:7043/api/products').subscribe({
-        next: (response) => { this.products = (response as IPagination).data },
-        error: (error) => { console.log(error) }
-      });
   }
 }
--- Presentation/src/app/app.module.ts
+++ Presentation/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     //AppRoutingModule,
     HttpClientModule,
-    CoreModule
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
